0cc64f1545329f7947e5d9cad5487c9ceb191f03
Rodolfo Rojas <jose.rojas@avantica.com>
2021-08-11 14:54:19 -0600

    Add ISO 8601 time format

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-  fmt.Println("hello world") 
+	fmt.Println("Time Now: ", time.Now().UTC().Format("2006-01-02T15:04:05Z07:00"))
 }
